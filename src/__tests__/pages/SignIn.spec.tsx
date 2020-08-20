392214aa6dc8dedee4a05ceb7f7b81b22eb27b32
denismend <denisladeira1@gmail.com>
2020-08-19 21:44:12 -0300

    tests

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -3,6 +3,12 @@ import { render } from '@testing-library/react-native';
 
 import SignIn from '../../pages/SignIn';
 
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: jest.fn(),
+  };
+});
+
 describe('SignIn page', () => {
   it('should contains email/password inputs', () => {
     const { getByPlaceholderText } = render(<SignIn />);
