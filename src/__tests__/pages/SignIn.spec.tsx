07e1c1e28c309aee4787306ab5b4d6ae54b26de2
denismend <denisladeira1@gmail.com>
2020-08-19 21:39:38 -0300

    tests

---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import SignIn from '../../pages/SignIn';
+
+describe('SignIn page', () => {
+  it('should contains email/password inputs', () => {
+    const { getByPlaceholderText } = render(<SignIn />);
+
+    expect(getByPlaceholderText('E-mail')).toBeTruthy();
+    expect(getByPlaceholderText('Senha')).toBeTruthy();
+  });
+});
